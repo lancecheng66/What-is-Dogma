a5034c66d8d437b0d53e7b694c913f7d9b65269f
lancecheng <lancecheng214@yahoo.com>
2018-06-12 00:43:14 +0800

    Stage 2 finished

---
+++ Heroes and Dogma/Assets/Changescenebox.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class Changescenebox : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Cutscene4");
+        }
+    }
+}
