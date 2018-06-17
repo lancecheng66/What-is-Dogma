73c80d7d9757423a976cb4cd5487280bb3b2d0ff
lancecheng <lancecheng214@yahoo.com>
2018-06-17 16:57:41 +0800

    Creating VS version of characters

--- Heroes and Dogma/Assets/CharacterSelect3.cs
+++ Heroes and Dogma/Assets/CharacterSelect3.cs
@@ -1,16 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class CharacterSelect3 : MonoBehaviour {
+public class CharacterSelect3 : MonoBehaviour
+{
+    
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private GameObject[] characterList;
+
+    private void Start()
+    {
+        characterList = new GameObject[transform.childCount];
+        // fill array with models
+        for (int i = 0; i < transform.childCount; i++)
+            characterList[i] = transform.GetChild(i).gameObject;
+        // turn off their renderer
+        foreach (GameObject go in characterList)
+            go.SetActive(false);
+    }
+
+
+    public void Kayla()
+    {
+        characterList[0].SetActive(true);
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    public void Jarred()
+    {
+        characterList[1].SetActive(true);
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    public void Sven()
+    {
+        characterList[2].SetActive(true);
+        DontDestroyOnLoad(transform.gameObject);
+    }
+    public void Anastasia()
+    {
+        characterList[3].SetActive(true);
+        DontDestroyOnLoad(transform.gameObject);
+    }
+    public void Lance()
+    {
+        characterList[4].SetActive(true);
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    public void Michael()
+    {
+        characterList[5].SetActive(true);
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    public void Kim()
+    {
+        characterList[6].SetActive(true);
+        DontDestroyOnLoad(transform.gameObject);
+    }
+    public void Iris()
+    {
+        characterList[7].SetActive(true);
+        DontDestroyOnLoad(transform.gameObject);
+    }
+    public void ConfirmButton()
+    {
+        SceneManager.LoadScene("Versus");
+    }
+
 }
---
+++ Heroes and Dogma/Assets/CutsceneEnd2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutsceneEnd2 : MonoBehaviour {
+
+    [SerializeField]
+    private float countdown;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(JumpToScene());
+    }
+
+    IEnumerator JumpToScene()
+    {
+        yield return new WaitForSeconds(countdown);
+        SceneManager.LoadScene("Character select 3");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
